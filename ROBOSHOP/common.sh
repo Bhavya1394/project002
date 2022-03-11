428d8d3ff464dde2dafe02b75da97bf7218c6c10
raghu_windows <raghukumarkamatham@gmail.com>
2022-03-11 18:53:06 +0530

    Adding changes in Common.sh file

--- ROBOSHOP/common.sh
+++ ROBOSHOP/common.sh
@@ -13,10 +13,12 @@ STAT_CHECK() {
     echo -e "\e[32m done\e[0m"
  else
     echo -e "\e[31m fail\e[0m"
+    echo -e "\e[33m Check the log file for more details, Log file - $LOG\e[0m"
     exit 1
  fi
 }
 
 PRINT() {
-    echo -n -e "$1\t\t..."
+  echo -e "\n########################\t$1\t#######################" &>>$LOG
+  echo -n -e "$1\t\t..."
 }
